221427448d31646cf96c8e914dd2aefe3eb05aaf
artem-dudarev <ad@virtoway.com>
2024-06-05 15:12:52 +0200

    Don't throw exception if token already exists

--- src/VirtoCommerce.PushMessages.ExperienceApi/Commands/AddFcmTokenCommandHandler.cs
+++ src/VirtoCommerce.PushMessages.ExperienceApi/Commands/AddFcmTokenCommandHandler.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
@@ -10,17 +11,31 @@ namespace VirtoCommerce.PushMessages.ExperienceApi.Commands;
 public class AddFcmTokenCommandHandler : IRequestHandler<AddFcmTokenCommand, bool>
 {
     private readonly IFcmTokenService _fcmTokenService;
+    private readonly IFcmTokenSearchService _fcmTokenSearchService;
 
-    public AddFcmTokenCommandHandler(IFcmTokenService fcmTokenService)
+    public AddFcmTokenCommandHandler(
+        IFcmTokenService fcmTokenService,
+        IFcmTokenSearchService fcmTokenSearchService)
     {
         _fcmTokenService = fcmTokenService;
+        _fcmTokenSearchService = fcmTokenSearchService;
     }
 
     public async Task<bool> Handle(AddFcmTokenCommand request, CancellationToken cancellationToken)
     {
-        var fcmToken = AbstractTypeFactory<FcmToken>.TryCreateInstance();
-        fcmToken.UserId = request.UserId;
-        fcmToken.Token = request.Token;
+        var searchCriteria = AbstractTypeFactory<FcmTokenSearchCriteria>.TryCreateInstance();
+        searchCriteria.Token = request.Token;
+        searchCriteria.UserIds = [request.UserId];
+
+        var searchResult = await _fcmTokenSearchService.SearchAsync(searchCriteria);
+        var fcmToken = searchResult.Results.FirstOrDefault();
+
+        if (fcmToken is null)
+        {
+            fcmToken = AbstractTypeFactory<FcmToken>.TryCreateInstance();
+            fcmToken.Token = request.Token;
+            fcmToken.UserId = request.UserId;
+        }
 
         await _fcmTokenService.SaveChangesAsync([fcmToken]);
 
